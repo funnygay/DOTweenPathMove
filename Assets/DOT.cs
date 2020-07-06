59b3a3774cd91b8eb3f2f2269485e215ce4e0ab8
guanghui-zhang <guanghui.zhang@unity3d.com>
2020-07-06 18:18:26 +0800

    free move

---
+++ Assets/DOT.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Drawing;
+
+public class DOT : MonoBehaviour
+{
+    public Vector3[] waypointsVec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Transform>().DOPath(waypointsVec, 20f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
